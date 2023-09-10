a8b2020b0f49151e17983fd8783ccecbdef16886
moncef2006 <73278962+moncef2006@users.noreply.github.com>
2023-09-10 22:40:31 +0100

    adding oreder & add to cart button

--- components/DetailsPage.jsx
+++ components/DetailsPage.jsx
@@ -1,20 +1,44 @@
 "use client";
 // import Image from "next/image";
 import React, { useState } from "react";
-import { AiOutlineLeft, AiOutlineDown } from "react-icons/ai";
+import {
+  AiOutlineLeft,
+  AiOutlineDown,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
 
 function DetailsPage({ book }) {
+  //description State
   const [showdescription, setShowDescription] = useState(false);
+
+  //qty State
+  const [QTY, setQTY] = useState(0);
+
+  const Decrease = () => {
+    if (QTY === 0) {
+      return;
+    } else {
+      setQTY((prev) => prev - 1);
+    }
+  };
+  const Increase = () => {
+    setQTY((prev) => prev + 1);
+  };
   return (
     <div className="flex flex-col md:flex-row w-full p-5 ">
+      {/* Image */}
       <div className="w-full ">
         <img src={book.image.url} alt={book.title} className="object-contain" />
       </div>
+
+      {/* Title & Price */}
       <div className="flex flex-col mt-3 w-full h-full gap-3">
         <div className="flex items-center justify-between w-full">
           <h1 className="text-xl font-bold text-black/80">{book.title}</h1>
           <h1 className="text-xl font-base ">{book.price}$</h1>
         </div>
+
+        {/* Detials */}
         <div className="">
           <div
             className="flex items-center  justify-between"
@@ -32,7 +56,9 @@ function DetailsPage({ book }) {
           {showdescription ? (
             <p
               className={
-                showdescription ? " duration-150" : "opacity-0 duration-150"
+                showdescription
+                  ? " duration-150 translate-y-0 opacity-100"
+                  : "opacity-0 duration-150 -translate-y-40"
               }
             >
               {book.description}
@@ -40,11 +66,40 @@ function DetailsPage({ book }) {
           ) : (
             <></>
           )}
-          {/* {showdescription ? (
-            <></>
-          ) : (
-            <div className="w-full h-[0.1px] bg-black/40 mt-3"></div>
-          )} */}
+        </div>
+
+        {/* Author */}
+        <div className="flex flex-col">
+          <h1 className="text-lg font-semibold">
+            Author:
+            <span className="text-base text-gray-700 font-normal">
+              {" "}
+              {book.author}
+            </span>
+          </h1>
+          <h1 className="text-lg font-semibold">
+            Job:
+            <span className="text-base text-gray-700 font-normal">
+              {" "}
+              {book.job}
+            </span>
+          </h1>
+        </div>
+        {/* QTY & Order */}
+        <div className="flex gap-5 justify-between">
+          <div className="flex border w-full justify-between p-4">
+            <button className="text-xl cursor-pointer" onClick={Decrease}>
+              -
+            </button>
+            <div className="text-xl">{QTY}</div>
+            <button className="text-xl cursor-pointer" onClick={Increase}>
+              +
+            </button>
+          </div>
+          <button className="bg-indigo-400 px-5 text-white text-lg  flex items-center justify-between w-full">
+            Add to Cart
+            <AiOutlineShoppingCart size={25} />
+          </button>
         </div>
       </div>
     </div>
